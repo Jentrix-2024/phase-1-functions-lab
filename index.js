f68116bcfa32a78c141176bdb6b080d3dad463a8
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-09-20 22:14:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceInblocks){
+ return distanceInblocks>=42?(distanceInblocks-42):(42-distanceInblocks);
+ }
+
+
+
+function distanceFromHqInFeet(pickupLocation){
+const distanceInFeet= distanceFromHqInBlocks(pickupLocation)*264
+return distanceInFeet;
+}
+function distanceTravelledInFeet(start,pickupLocation){
+    return start>pickupLocation?(start-pickupLocation)*264:(pickupLocation-start)*264
+}
+function calculatesFarePrice(start,pickupLocation){
+    const distance=distanceTravelledInFeet(start,pickupLocation)
+    {
+    if (distance<=400)
+    return 0;
+    else if 
+    (distance<2000,distance>400)
+    return(distance-400)*0.02;
+     else if 
+    (distance>2000,distance<2500)
+    return $25;
+    else 
+     return'cannot travel that far';
+    }
+}
